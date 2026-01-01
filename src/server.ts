e76e27fbc50509437d9729242e63351160b4a62a
Natnael Tamiru <natnaeltamiru98@gmail.com>
2026-01-01 17:39:44 +0300

    fix: update the server health checker

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,8 @@
-import app from "./app";
 import "dotenv/config";
+import app from "./app.js";
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log(`Server is running on $https://localhost:{PORT}`);
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
