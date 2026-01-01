5b6cb24e80956e1d54366dfae2f8094c0ad49206
Natnael Tamiru <natnaeltamiru98@gmail.com>
2026-01-01 16:41:34 +0300

    chore: update the compiler tsconfig.json

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,13 @@
 import express from "express";
-
+import "dotenv/config";
 const app = express();
+
+const PORT = process.env.PORT;
+
+app.get("/", (req, res) => {
+  res.send("This is from Backend");
+});
+
+app.listen(3000, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
