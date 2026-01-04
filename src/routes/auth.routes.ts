c0044222930c497a0a5ca4ec5fe01b09258a83f7
Natnael Tamiru <natnaeltamiru98@gmail.com>
2026-01-04 22:14:02 +0300

    chore: mount on auth routes

---
+++ src/routes/auth.routes.ts
@@ -0,0 +1,9 @@
+import { Router } from "express";
+import { AuthController } from "../controllers/auth.controller.js";
+
+const router = Router();
+
+router.post("/signup", AuthController.register);
+router.post("/signin", AuthController.login);
+
+export default router;
