d19a3e9056279f920c4f409a7f74acbb058c55ee
Natnael Tamiru <natnaeltamiru98@gmail.com>
2026-01-04 03:39:22 +0300

    chore: refine password and confirm password

--- src/validators/auth.validator.ts
+++ src/validators/auth.validator.ts
@@ -1,12 +1,17 @@
 import { z } from "zod";
 
-export const signUpSchema = z.object({
-  fullName: z.string().min(3),
-  phoneNumber: z.string().min(10).max(13),
-  team: z.string().optional().nullable(),
-  department: z.string().optional().nullable(),
-  yearOfStudy: z.number().int().positive().optional().nullable(),
-  telegramUserName: z.string().optional().nullable(),
-  password: z.string().min(6),
-  confirmPassword: z.string().min(6),
-});
+export const signUpSchema = z
+  .object({
+    fullName: z.string().min(3),
+    phoneNumber: z.string().min(10).max(13),
+    team: z.string().optional().nullable(),
+    department: z.string().optional().nullable(),
+    yearOfStudy: z.number().int().positive().optional().nullable(),
+    telegramUserName: z.string().optional().nullable(),
+    password: z.string().min(6),
+    confirmPassword: z.string().min(6),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    message: "Passwords do not match",
+    path: ["confirmPassword"],
+  });
