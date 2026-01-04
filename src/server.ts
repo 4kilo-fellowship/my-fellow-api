f1b6301dd0287534ae792fdf07afa036343e3ff1
Natnael Tamiru <natnaeltamiru98@gmail.com>
2026-01-04 22:28:48 +0300

    chore: connect with mongodb

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,20 @@
 import "dotenv/config";
 import app from "./app.js";
+import mongoose from "mongoose";
 
 const PORT = process.env.PORT || 4000;
+const MONGO_URI = process.env.MONGO_URI;
+
+mongoose
+  .connect(MONGO_URI)
+  .then(() => {
+    console.log("MongoDB connected");
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+  })
+  .catch((err) => {
+    console.error("Mongo connection error:", err);
+    process.exit(1);
+  });
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
