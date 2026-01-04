165df797c85ec7ceb8c52a11b53612cd59b760dc
Natnael Tamiru <natnaeltamiru98@gmail.com>
2026-01-04 03:36:45 +0300

    chore: add sign up schema using zod

---
+++ src/validators/auth.validator.ts
@@ -0,0 +1,12 @@
+import { z } from "zod";
+
+export const signUpSchema = z.object({
+  fullName: z.string().min(3),
+  phoneNumber: z.string().min(10).max(13),
+  team: z.string().optional().nullable(),
+  department: z.string().optional().nullable(),
+  yearOfStudy: z.number().int().positive().optional().nullable(),
+  telegramUserName: z.string().optional().nullable(),
+  password: z.string().min(6),
+  confirmPassword: z.string().min(6),
+});
