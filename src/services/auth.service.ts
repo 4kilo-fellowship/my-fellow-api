5b111d75fab29ea9a3869842c3ab44127a34c830
Natnael Tamiru <natnaeltamiru98@gmail.com>
2026-01-04 16:22:47 +0300

    chore: login the user

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -20,7 +20,33 @@ export class AuthService {
     });
 
     await user.save();
-    token = signJwt({ sub: user._id, phoneNumber: user.phoneNumber });
+    const token = signJwt({ sub: user._id, phoneNumber: user.phoneNumber });
+
+    const safeUser = {
+      id: user._id,
+      fullName: user.fullName,
+      phoneNumber: user.phoneNumber,
+      team: user.team,
+      department: user.department,
+      yearOfStudy: user.yearOfStudy,
+      telegramUserName: user.telegramUserName,
+      createdAt: user.createdAt,
+    };
+
+    return { user: safeUser, token };
+  }
+
+  static async login(dto: SignInDTO) {
+    const user = await UserModel.findOne({ phoneNumber: dto.phoneNumber });
+    if (!user) {
+      throw new Error("Invalid credentials");
+    }
+
+    const valid = await user.comparePassword(dto.password);
+    if (!valid) {
+      throw new Error("Invalid credentials");
+    }
+    const token = signJwt({ sub: user._id, phoneNumber: user.phoneNumber });
 
     const safeUser = {
       id: user._id,
